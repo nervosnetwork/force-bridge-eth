c10bb6ea588c335947ca8d345c95a549ade40dd9
vincent <vyclzn@gmail.com>
2020-10-27 15:36:03 +0800

    chore: add docker

---
+++ docker/ckb/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if ! [ -f ckb.toml ]; then
+  /bin/ckb init --chain "$CKB_CHAIN" --ba-arg "$BA_ARG" --ba-code-hash "$BA_CODE_HASH" --ba-hash-type "$BA_HASH_TYPE" --ba-message "$BA_MESSAGE"
+fi
+
+exec /bin/ckb run &
+sleep 3 
+exec /bin/ckb miner 
