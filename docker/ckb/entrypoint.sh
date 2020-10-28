02c5d2dad5a18a939cebbfa907181330ecac584a
vincent <vyclzn@gmail.com>
2020-10-28 10:57:55 +0800

    chore: remove redirect

--- docker/ckb/entrypoint.sh
+++ docker/ckb/entrypoint.sh
@@ -4,11 +4,6 @@ if ! [ -f ckb.toml ]; then
   /bin/ckb init --chain "$CKB_CHAIN" --ba-arg "$BA_ARG" --ba-code-hash "$BA_CODE_HASH" --ba-hash-type "$BA_HASH_TYPE" --ba-message "$BA_MESSAGE"
 fi
 
-mkdir ckb_log \
-	&& touch ckb_log/output.log ckb_log/error.log \
-	&& ln -sf /dev/stdout ./ckb_log/output.log \
-  && ln -sf /dev/stderr ./ckb_log/error.log
-
-exec /bin/ckb run > ./ckb_log/output.log >> ./ckb_log/error.log &
+exec /bin/ckb run &
 sleep 3
 exec /bin/ckb miner
