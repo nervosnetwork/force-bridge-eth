93a80868accc57a5dfc60d631aebb284014517ff
fpChan <fpchen@outlook.com>
2020-11-03 18:34:20 +0800

    fix: add relay logic && format code

--- offchain-modules/lib/src/transfer/to_eth.rs
+++ offchain-modules/lib/src/transfer/to_eth.rs
@@ -1,4 +1,5 @@
 use crate::util::ckb_util::{covert_to_h256, make_ckb_transaction};
+use anyhow::anyhow;
 use anyhow::Result;
 use ckb_sdk::rpc::{BlockView, TransactionView};
 use ckb_sdk::{AddressPayload, HttpRpcClient, SECP256K1};
@@ -12,7 +13,6 @@ use force_sdk::util::{parse_privkey_path, send_tx_sync};
 use log::debug;
 use serde::export::Clone;
 use serde::{Deserialize, Serialize};
-use std::collections::HashSet;
 
 // tx_merkle_index == index in transactions merkle tree of the block
 #[derive(Clone, Default, Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
@@ -27,19 +27,23 @@ pub struct CkbTxProof {
 
 pub fn burn(private_key: String, rpc_url: String) -> Result<String> {
     let mut rpc_client = HttpRpcClient::new(rpc_url);
-    let from_privkey = parse_privkey_path(private_key.as_str())?;
+    let from_privkey = parse_privkey_path(&private_key)?;
     let from_public_key = secp256k1::PublicKey::from_secret_key(&SECP256K1, &from_privkey);
     let address_payload = AddressPayload::from_pubkey(&from_public_key);
     let from_lockscript = Script::from(&address_payload);
-    let unsigned_tx = make_ckb_transaction(from_lockscript).unwrap();
-    let tx = sign(unsigned_tx, &mut rpc_client, &from_privkey).unwrap();
+    let unsigned_tx = make_ckb_transaction(from_lockscript)?;
+    let tx = sign(unsigned_tx, &mut rpc_client, &from_privkey)
+        .map_err(|e| anyhow!("failed to sign tx : {}", e))?;
     log::info!(
         "tx: \n{}",
-        serde_json::to_string_pretty(&ckb_jsonrpc_types::TransactionView::from(tx.clone()))
-            .unwrap()
+        serde_json::to_string_pretty(&ckb_jsonrpc_types::TransactionView::from(tx.clone()))?
     );
     send_tx_sync(&mut rpc_client, &tx, 60).map_err(|e| anyhow::anyhow!(e))?;
-    let cell_typescript = tx.output(0).unwrap().type_().to_opt();
+    let cell_typescript = tx
+        .output(0)
+        .ok_or_else(|| anyhow::anyhow!("first output cell is none"))?
+        .type_()
+        .to_opt();
     let cell_script = match cell_typescript {
         Some(script) => hex::encode(script.as_slice()),
         None => "".to_owned(),
@@ -48,7 +52,7 @@ pub fn burn(private_key: String, rpc_url: String) -> Result<String> {
         "tx_hash": hex::encode(tx.hash().as_slice()),
         "cell_typescript": cell_script,
     });
-    debug!("{}", serde_json::to_string_pretty(&print_res).unwrap());
+    debug!("{}", serde_json::to_string_pretty(&print_res)?);
     Ok(hex::encode(tx.hash().as_slice()))
 }
 
@@ -59,9 +63,9 @@ pub fn unlock() -> Result<()> {
 pub fn get_add_ckb_headers_func() -> Function {
     //TODO : addHeader is mock function for test feature which set header data in eth contract
     Function {
-        name: "addHeader".to_owned(),
+        name: "addHeaders".to_owned(),
         inputs: vec![Param {
-            name: "_data".to_owned(),
+            name: "data".to_owned(),
             kind: ParamType::Bytes,
         }],
         outputs: vec![],
@@ -72,32 +76,24 @@ pub fn get_add_ckb_headers_func() -> Function {
 pub fn parse_ckb_proof(tx_hash_str: &str, rpc_url: String) -> Result<CkbTxProof> {
     let tx_hash = covert_to_h256(tx_hash_str)?;
     let mut rpc_client = HttpRpcClient::new(rpc_url);
-    let mut tx_indices = HashSet::new();
-    let retrieved_block_hash;
-    let retrieved_block;
-    let tx_index;
-    let tx_with_status_opt = rpc_client
+    let retrieved_block_hash = rpc_client
         .get_transaction(tx_hash.clone())
-        .map_err(|e| anyhow::anyhow!("failed to get ckb tx: {}", e))?;
+        .map_err(|e| anyhow!("failed to get ckb tx: {}", e))?
+        .ok_or_else(|| anyhow!("Transaction {:#x} not exists", tx_hash))?
+        .tx_status
+        .block_hash
+        .ok_or_else(|| anyhow!("Transaction {:#x} not yet in block", tx_hash))?;
 
-    match tx_with_status_opt {
-        Some(tx_with_status) => retrieved_block_hash = tx_with_status.tx_status.block_hash,
-        None => anyhow::bail!(format!("Transaction {:#x} not yet in block", tx_hash)),
-    }
-    retrieved_block = rpc_client
-        .get_block(retrieved_block_hash.clone().expect("tx_block_hash is none"))
+    let retrieved_block = rpc_client
+        .get_block(retrieved_block_hash.clone())
         .map_err(|e| anyhow::anyhow!("failed to get ckb block: {}", e))?
-        .expect("block is none");
+        .ok_or_else(|| anyhow!("block is none"))?;
 
-    tx_index =
-        get_tx_index(&tx_hash, &retrieved_block).expect("tx_hash not in retrieved_block") as u32;
+    let tx_index = get_tx_index(&tx_hash, &retrieved_block)
+        .ok_or_else(|| anyhow!("tx_hash not in retrieved_block"))? as u32;
+    let tx_indices = vec![tx_index];
     debug!("tx index: {}", tx_index);
-    if !tx_indices.insert(tx_index) {
-        anyhow::bail!("Duplicated tx_hash {:#x}", tx_hash)
-    }
-
     let tx_num = retrieved_block.transactions.len();
-    let retrieved_block_hash = retrieved_block_hash.expect("checked len");
     debug!("retrieved block hash {:?}", retrieved_block_hash);
     debug!("retrieved header hash {:?}", retrieved_block.header.hash);
 
@@ -107,9 +103,9 @@ pub fn parse_ckb_proof(tx_hash_str: &str, rpc_url: String) -> Result<CkbTxProof>
             .iter()
             .map(|tx| tx.hash.pack())
             .collect::<Vec<_>>(),
-        &tx_indices.into_iter().collect::<Vec<_>>(),
+        &tx_indices,
     )
-    .expect("build proof with verified inputs should be OK");
+    .ok_or_else(|| anyhow!("build proof with verified inputs should be OK"))?;
 
     // tx_merkle_index means the tx index in transactions merkle tree of the block
     Ok(CkbTxProof {
