fc813dfc7cb49cd7d17e6e585506070d1905f960
Ethan Hu <cosinlink@163.com>
2020-11-03 09:56:39 +0800

    fix: add prettier instead of eslint to format js code

--- eth-contracts/test/TokenLocker.test.js
+++ eth-contracts/test/TokenLocker.test.js
@@ -1,100 +1,118 @@
-const { expect } = require('chai')
-const { log, waitingForGasUsed } = require('./utils')
+const { expect } = require("chai");
+const { log, waitingForGasUsed } = require("./utils");
 
-contract('TokenLocker', () => {
-  let tokenLocker, provider, user
+contract("TokenLocker", () => {
+  let tokenLocker, provider, user;
 
   before(async function () {
     // disable timeout
-    this.timeout(0)
-    let factory = await ethers.getContractFactory("contracts/MockCKBSpv.sol:MockCKBSpv")
-    const mockSpv = await factory.deploy()
-    await mockSpv.deployed()
+    this.timeout(0);
+    let factory = await ethers.getContractFactory(
+      "contracts/MockCKBSpv.sol:MockCKBSpv"
+    );
+    const mockSpv = await factory.deploy();
+    await mockSpv.deployed();
 
-    factory = await ethers.getContractFactory("contracts/TokenLocker.sol:TokenLocker")
-    tokenLocker = await factory.deploy(mockSpv.address, 123)
-    await tokenLocker.deployed()
+    factory = await ethers.getContractFactory(
+      "contracts/TokenLocker.sol:TokenLocker"
+    );
+    tokenLocker = await factory.deploy(mockSpv.address, 123);
+    await tokenLocker.deployed();
 
-    user = tokenLocker.signer
-    provider = tokenLocker.provider
-  })
+    user = tokenLocker.signer;
+    provider = tokenLocker.provider;
+  });
 
-  describe('lockETH', async function () {
+  describe("lockETH", async function () {
     // disable timeout
-    this.timeout(0)
-    it('Should lockETH verified', async () => {
+    this.timeout(0);
+    it("Should lockETH verified", async () => {
       // let defaultProvider = ethers.getDefaultProvider()
-      const contractBalance = await provider.getBalance(tokenLocker.address)
+      const contractBalance = await provider.getBalance(tokenLocker.address);
 
       // lockETH
-      const amount = ethers.utils.parseEther('1.2')
-      const res = await tokenLocker.lockETH(0, { value: amount })
-      const gasUsed = await waitingForGasUsed(provider, res)
-      log(`gasUsed: ${gasUsed.toString()}`)
+      const amount = ethers.utils.parseEther("1.2");
+      const res = await tokenLocker.lockETH(0, { value: amount });
+      const gasUsed = await waitingForGasUsed(provider, res);
+      log(`gasUsed: ${gasUsed.toString()}`);
 
       // asset expected amount == balance of contract delta
-      const delta = await provider.getBalance(tokenLocker.address) - contractBalance
-      const actualDelta = ethers.BigNumber.from(delta.toString())
-      expect(actualDelta).to.equal(amount)
-    })
-  })
+      const delta =
+        (await provider.getBalance(tokenLocker.address)) - contractBalance;
+      const actualDelta = ethers.BigNumber.from(delta.toString());
+      expect(actualDelta).to.equal(amount);
+    });
+  });
 
-  describe('lockToken', async function () {
+  describe("lockToken", async function () {
     // disable timeout
-    this.timeout(0)
-    it('Should lock erc20 token verified', async () => {
+    this.timeout(0);
+    it("Should lock erc20 token verified", async () => {
       // deploy erc20
-      const factory = await ethers.getContractFactory("contracts/test/ERC20.sol:ERC20")
-      const erc20 = await factory.deploy()
-      await erc20.deployed()
+      const factory = await ethers.getContractFactory(
+        "contracts/test/ERC20.sol:ERC20"
+      );
+      const erc20 = await factory.deploy();
+      await erc20.deployed();
       log("erc20 deployed to:", erc20.address);
 
       // get balance
-      const contractBalance = await erc20.callStatic.balanceOf(tokenLocker.address)
-      log(`tokenLocker contract erc20 balance: ${contractBalance.toString()}`)
-      const signerBalance = await erc20.callStatic.balanceOf(await user.getAddress())
-      log(`signer erc20 balance: ${signerBalance.toString()}`)
+      const contractBalance = await erc20.callStatic.balanceOf(
+        tokenLocker.address
+      );
+      log(`tokenLocker contract erc20 balance: ${contractBalance.toString()}`);
+      const signerBalance = await erc20.callStatic.balanceOf(
+        await user.getAddress()
+      );
+      log(`signer erc20 balance: ${signerBalance.toString()}`);
 
       // user should approve erc20 token to tokenLocker contract
-      const approveAmount = '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
-      let res = await erc20.approve(tokenLocker.address, approveAmount)
-      let gasUsed = await waitingForGasUsed(provider, res)
-      log(`approve gasUsed: ${gasUsed.toString()}`)
+      const approveAmount =
+        "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+      let res = await erc20.approve(tokenLocker.address, approveAmount);
+      let gasUsed = await waitingForGasUsed(provider, res);
+      log(`approve gasUsed: ${gasUsed.toString()}`);
 
       // lock erc20
-      const amount = 789
-      log(`lock erc20 token amount ${amount}`)
-      res = await tokenLocker.lockToken(erc20.address, amount, "ckb_address")
-      gasUsed = await waitingForGasUsed(provider, res)
-      log(`gasUsed: ${gasUsed.toString()}`)
+      const amount = 789;
+      log(`lock erc20 token amount ${amount}`);
+      res = await tokenLocker.lockToken(erc20.address, amount, "ckb_address");
+      gasUsed = await waitingForGasUsed(provider, res);
+      log(`gasUsed: ${gasUsed.toString()}`);
 
       // asset expected amount == balance of contract delta
-      const contractBalanceAfter = await erc20.callStatic.balanceOf(tokenLocker.address)
-      log(`contractBalanceAfter ${contractBalanceAfter}`)
-      const delta = contractBalanceAfter - contractBalance
-      const actualDelta = ethers.BigNumber.from(delta.toString())
-      expect(actualDelta).to.equal(amount)
-    })
-  })
+      const contractBalanceAfter = await erc20.callStatic.balanceOf(
+        tokenLocker.address
+      );
+      log(`contractBalanceAfter ${contractBalanceAfter}`);
+      const delta = contractBalanceAfter - contractBalance;
+      const actualDelta = ethers.BigNumber.from(delta.toString());
+      expect(actualDelta).to.equal(amount);
+    });
+  });
 
-  describe('unlockETH', async function () {
+  describe("unlockETH", async function () {
     // disable timeout
-    this.timeout(0)
-    it('Should unlockETH verified', async () => {
+    this.timeout(0);
+    it("Should unlockETH verified", async () => {
       // let defaultProvider = ethers.getDefaultProvider()
-      const defaultProvider = tokenLocker.signer.provider
-      const contractBalance = await defaultProvider.getBalance(tokenLocker.address)
+      const defaultProvider = tokenLocker.signer.provider;
+      const contractBalance = await defaultProvider.getBalance(
+        tokenLocker.address
+      );
 
       // unlockETH
-      const res = await tokenLocker.unlockToken([0], [0])
-      const gasUsed = await waitingForGasUsed(provider, res)
-      log(`gasUsed: ${gasUsed.toString()}`)
+      const res = await tokenLocker.unlockToken([0], [0]);
+      const gasUsed = await waitingForGasUsed(provider, res);
+      log(`gasUsed: ${gasUsed.toString()}`);
 
       // asset expected amount == balance of contract delta
-      const delta = await defaultProvider.getBalance(tokenLocker.address) - contractBalance
-      const actualDelta = ethers.BigNumber.from(delta.toString())
-      const expected = ethers.BigNumber.from('-111100000000000000')
-      expect(actualDelta).to.equal(expected)
-    })
-  })
-})
+      const delta =
+        (await defaultProvider.getBalance(tokenLocker.address)) -
+        contractBalance;
+      const actualDelta = ethers.BigNumber.from(delta.toString());
+      const expected = ethers.BigNumber.from("-111100000000000000");
+      expect(actualDelta).to.equal(expected);
+    });
+  });
+});
--- eth-contracts/test/utils.js
+++ eth-contracts/test/utils.js
@@ -1,25 +1,25 @@
 async function sleep(seconds) {
   // console.log(`waiting for block confirmations, about ${seconds}s`)
-  await new Promise(resolve => setTimeout(resolve, seconds * 1000))
+  await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
 async function waitingForGasUsed(provider, res) {
   if (!res) {
-    return -1
+    return -1;
   }
 
-  const txHash = res.hash
-  let txReceipt
+  const txHash = res.hash;
+  let txReceipt;
   while (!txReceipt) {
-    txReceipt = await provider.getTransactionReceipt(txHash)
+    txReceipt = await provider.getTransactionReceipt(txHash);
     if (txReceipt && txReceipt.blockHash) {
-      break
+      break;
     }
-    await sleep(1)
+    await sleep(1);
   }
-  return txReceipt.gasUsed
+  return txReceipt.gasUsed;
 }
 
-const { log } = console
+const { log } = console;
 
-module.exports = { sleep, log, waitingForGasUsed }
+module.exports = { sleep, log, waitingForGasUsed };
