62decc64f1dfe98b7a6db8521abbad70dfc0b54c
LeonLi000 <matrix.skygirl@gmail.com>
2020-11-04 23:38:24 +0800

    chore: code clean

--- offchain-modules/lib/src/relay/eth_relay.rs
+++ offchain-modules/lib/src/relay/eth_relay.rs
@@ -7,9 +7,8 @@ use ckb_types::packed::{Byte32, Script};
 use ckb_types::prelude::{Builder, Entity};
 use ethereum_types::H256;
 use force_sdk::cell_collector::get_live_cell_by_typescript;
-use futures::future::BoxFuture;
-use futures::FutureExt;
-use web3::types::{Block, BlockHeader, BlockId};
+use std::ops::Add;
+use web3::types::{Block, BlockHeader, BlockId, BlockNumber};
 
 pub struct ETHRelayer {
     pub config_path: String,
@@ -66,7 +65,8 @@ impl ETHRelayer {
                 // Determine whether the latest_header is on the Ethereum main chain
                 // If it is in the main chain, the new header currently needs to be added current_height = latest_height + 1
                 // If it is not in the main chain, it means that reorg has occurred, and you need to trace back from latest_height until the back traced header is on the main chain
-                lookup_common_ancestor(headers, index, &mut rpc_client);
+                let block = lookup_common_ancestor(headers, index, &mut rpc_client).await?;
+                do_relay_loop(&mut rpc_client, block).await?;
             }
             None => anyhow::bail!("the bridge cell is not found."),
         }
@@ -75,60 +75,39 @@ impl ETHRelayer {
 }
 
 // Find the common ancestor of the latest header and main chain
-fn lookup_common_ancestor(
+pub async fn lookup_common_ancestor(
     headers: Vec<BlockHeader>,
-    index: usize,
+    mut index: usize,
     rpc_client: &mut Web3Client,
-) -> BoxFuture<()> {
-    async move {
+) -> Result<Block<H256>> {
+    while index > 0 {
         let latest_header = &headers[index];
         let block = rpc_client
             .get_block(BlockId::Hash(latest_header.hash.unwrap()))
             .await;
         match block {
             Ok(block) => {
-                // The latest header on the ckb contract is on the Ethereum main chain, and goes directly to the normal loop logic
-                do_relay_loop(block).await.unwrap();
+                return Ok(block);
             }
             Err(_) => {
                 // The latest header on ckb is not on the Ethereum main chain and needs to be backtracked
-                if index > 1 {
-                    lookup_common_ancestor(headers, index - 1, rpc_client).await;
-                } else {
-                    panic!("system error! can not find the common ancestor with main chain.")
-                }
+                index -= 1;
             }
         }
     }
-    .boxed()
+    Err(anyhow::Error::msg(
+        "system error! can not find the common ancestor with main chain.",
+    ))
 }
 
-// 查找 latest header 和 main chain 的共同祖先
-// pub async fn lookup_common_ancestor(
-//     headers: Vec<BlockHeader>,
-//     index: usize,
-//     rpc_client: &mut Web3Client,
-// ) -> Result<()> {
-//     let latest_header = &headers[index];
-//     let block = rpc_client
-//         .get_block(BlockId::Hash(latest_header.hash.unwrap()))
-//         .await;
-//     match block {
-//         Ok(block) => {
-//             // ckb contract 上最新的 header 在以太坊 main chain上，直接走正常 loop 逻辑
-//             do_relay_loop(block).await?;
-//         }
-//         Err(_) => {
-//             // ckb 上最新的 header 不在以太坊 main chain 上，需要回溯
-//             lookup_common_ancestor(headers, index - 1, rpc_client).await?;
-//         }
-//     }
-//     Ok(())
-// }
-
-pub async fn do_relay_loop(block: Block<H256>) -> Result<()> {
-    let _number = block.number;
-    Ok(())
+pub async fn do_relay_loop(rpc_client: &mut Web3Client, block: Block<H256>) -> Result<()> {
+    let number = block.number.unwrap();
+    loop {
+        let block_id = BlockId::Number(BlockNumber::Number((number.as_u64().add(1)).into()));
+        let _new_header_rlp = rpc_client.get_header_rlp(block_id).await?;
+        // send ckb tx
+        std::thread::sleep(std::time::Duration::from_secs(2));
+    }
 }
 
 pub fn parse_headers(_data: &[u8]) -> Result<Vec<BlockHeader>> {
