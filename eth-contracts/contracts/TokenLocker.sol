d93aa4969ac9c0dd7837e9d88ff62ad520baa7d3
vincent <vyclzn@gmail.com>
2020-11-05 12:34:19 +0800

    test: add decode burn-tx tests

--- eth-contracts/contracts/TokenLocker.sol
+++ eth-contracts/contracts/TokenLocker.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.10;
+pragma experimental ABIEncoderV2;
 
 import "./interfaces/IERC20.sol";
 import {CKBCrypto} from "./libraries/CKBCrypto.sol";
@@ -12,15 +13,21 @@ import {ICKBSpv} from "./interfaces/ICKBSpv.sol";
 contract TokenLocker {
     using SafeMath for uint256;
     using Address for address;
-
     using TypedMemView for bytes;
     using TypedMemView for bytes29;
     using CKBTxView for bytes29;
     using ViewSpv for bytes29;
 
+    struct BurnResult {
+        uint256 amount;
+        address token;
+        address recipient;
+    }
+
     uint64 public numConfirmations_;
     ICKBSpv public ckbSpv_;
-    bytes32 public recipientCellTypeCodeHash_;
+    bytes32 public recipientCellTypescriptCodeHash_;
+    uint8 public recipientCellTypescriptHashType_;
 
     // txHash -> Used
     mapping(bytes32 => bool) public usedTx_;
@@ -28,80 +35,63 @@ contract TokenLocker {
     event Locked(
         address indexed token,
         address indexed sender,
+        string  indexed ckbAddress,
         uint256 amount,
-        string ckbAddress,
         string replayResistOutpoint
     );
 
     event Unlocked(
         address indexed token,
-        uint256 amount,
-        address recipient
+        address indexed recipient,
+        uint256 amount
     );
 
-    // Function output from burning fungible token on Near side.
-    struct BurnResult {
-        uint256 amount;
-        address token;
-        address recipient;
-    }
-
-    constructor(address ckbSpvAddress, uint64 numConfirmations, bytes32 recipientCellTypeCodeHash) public {
+    constructor(address ckbSpvAddress, uint64 numConfirmations, bytes32 typescriptCodeHash, uint8 typescriptHashType) public {
         ckbSpv_ = ICKBSpv(ckbSpvAddress);
         numConfirmations_ = numConfirmations;
-        recipientCellTypeCodeHash_ = recipientCellTypeCodeHash;
-    }
-
-    function _decodeBurnResult(bytes memory ckbTx) internal view returns (BurnResult memory) {
-        // TODO
-        // 1. check if ckbTx matches  ckbTxProof.txHash
-        // 2. _decodeBurnResult from ckbTx
-//        uint128 mockAmount = 111100000000000000;
-//        address mockToken = address(0);
-//        address mockRecipient = address(0);
-//        result = BurnResult(mockAmount, mockToken, mockRecipient);
-
-        bytes29 rawTx = ckbTx.ref(uint40(CKBTxView.CKBTxTypes.RawTx));
-        bytes29 recipientCellTypescript = rawTx.outputs().recipientCellOutput().typescript();
-        require((recipientCellTypescript.codeHash() == recipientCellTypeCodeHash_), "invalid recipient cell typescript code hash");
-        require((recipientCellTypescript.hashType() == 0), "invalid recipient cell typescript hash type");
-        require((recipientCellTypescript.args() == address(this)), "invalid recipient cell typescript hash type");
-        bytes29 recipientCellData = rawTx.outputsData().recipientCellData();
-        return BurnResult(recipientCellData.tokenAmount(), recipientCellData.tokenAddress(), recipientCellData.recipientAddress());
+        recipientCellTypescriptCodeHash_ = typescriptCodeHash;
+        recipientCellTypescriptHashType_ = typescriptHashType;
     }
 
     // before lockToken, user should approve -> TokenLocker Contract with 0xffffff token
     function lockToken(address token, uint256 amount, string memory ckbAddress, string memory replayResistOutpoint) public {
         // TODO modify `transferFrom` to `safeTransferFrom`
         IERC20(token).transferFrom(msg.sender, address(this), amount);
-        emit Locked(token, msg.sender, amount, ckbAddress, replayResistOutpoint);
+        emit Locked(token, msg.sender, ckbAddress, amount, replayResistOutpoint);
     }
 
     function lockETH(string memory ckbAddress, string memory replayResistOutpoint) public payable {
-        emit Locked(address(0), msg.sender, msg.value, ckbAddress, replayResistOutpoint);
+        emit Locked(address(0), msg.sender, ckbAddress, msg.value, replayResistOutpoint);
     }
 
     function unlockToken(bytes memory ckbTxProof, bytes memory ckbTx) public {
         require(ckbSpv_.proveTxExist(ckbTxProof, numConfirmations_), "tx from proofData should exist");
 
-        // Unpack the proof and extract the execution outcome.
         bytes29 proof = ckbTxProof.ref(uint40(ViewSpv.SpvTypes.CKBTxProof));
-
-        // TODO modify `mockTxHash` to `txHash`
         bytes32 txHash = proof.txHash();
         require(!usedTx_[txHash], "The burn tx cannot be reused");
         usedTx_[txHash] = true;
         require((txHash == CKBCrypto.digest(ckbTx, ckbTx.length)), "ckbTx mismatched with ckbTxProof");
 
-        BurnResult memory result = _decodeBurnResult(ckbTx);
+        BurnResult memory result = decodeBurnResult(ckbTx);
         // TODO modify `transfer` to `safeTransfer`
+        // if token == ETH
         if (result.token == address(0)) {
-            // it means token == Eth
             result.recipient.toPayable().transfer(result.amount);
         } else {
             IERC20(result.token).transfer(result.recipient, result.amount);
         }
 
-        emit Unlocked(address(result.token), result.amount, result.recipient);
+        emit Unlocked(address(result.token), result.recipient, result.amount);
+    }
+
+    function decodeBurnResult(bytes memory ckbTx) public view returns (BurnResult memory result) {
+        bytes29 rawTx = ckbTx.ref(uint40(CKBTxView.CKBTxTypes.RawTx));
+        bytes29 recipientCellTypescript = rawTx.outputs().recipientCellOutput().typescript();
+        require((recipientCellTypescript.codeHash() == recipientCellTypescriptCodeHash_), "invalid recipient cell typescript code hash");
+        require((recipientCellTypescript.hashType() == 0), "invalid recipient cell typescript hash type");
+        require((recipientCellTypescript.args() == address(this)), "invalid recipient cell typescript args");
+        bytes29 recipientCellData = rawTx.outputsData().recipientCellData();
+        result = BurnResult(recipientCellData.tokenAmount(), recipientCellData.tokenAddress(), recipientCellData.recipientAddress());
     }
 }
--- eth-contracts/test/TokenLocker.test.js
+++ eth-contracts/test/TokenLocker.test.js
@@ -1,12 +1,19 @@
 const { expect } = require("chai");
-const { log, waitingForGasUsed } = require("./utils");
+const { log, waitingForReceipt } = require("./utils");
+const testJson = require("./data/testTokenLocker.json");
+
+const recipientCellTypescript = testJson.recipientCellTypescript;
+const decodeBurnTxTestCases = testJson.decodeBurnTxTestCases;
+const lockETHTestCases = testJson.lockETHTestCases;
+const lockTokenTestCases = testJson.lockTokenTestCases;
+
+let tokenLocker, provider, user;
 
 contract("TokenLocker", () => {
-  let tokenLocker, provider, user;
-
   before(async function () {
     // disable timeout
     this.timeout(0);
+
     let factory = await ethers.getContractFactory(
       "contracts/MockCKBSpv.sol:MockCKBSpv"
     );
@@ -16,8 +23,14 @@ contract("TokenLocker", () => {
     factory = await ethers.getContractFactory(
       "contracts/TokenLocker.sol:TokenLocker"
     );
-    tokenLocker = await factory.deploy(mockSpv.address, 123);
+    tokenLocker = await factory.deploy(
+      mockSpv.address,
+      123,
+      recipientCellTypescript.codeHash,
+      recipientCellTypescript.hashType
+    );
     await tokenLocker.deployed();
+    log("tokenLocker deployed to:", tokenLocker.address);
 
     user = tokenLocker.signer;
     provider = tokenLocker.provider;
@@ -27,20 +40,9 @@ contract("TokenLocker", () => {
     // disable timeout
     this.timeout(0);
     it("Should lockETH verified", async () => {
-      // let defaultProvider = ethers.getDefaultProvider()
-      const contractBalance = await provider.getBalance(tokenLocker.address);
-
-      // lockETH
-      const amount = ethers.utils.parseEther("1.2");
-      const res = await tokenLocker.lockETH("ckbAddress", "replayResistOutpoint", { value: amount });
-      const gasUsed = await waitingForGasUsed(provider, res);
-      log(`gasUsed: ${gasUsed.toString()}`);
-
-      // asset expected amount == balance of contract delta
-      const delta =
-        (await provider.getBalance(tokenLocker.address)) - contractBalance;
-      const actualDelta = ethers.BigNumber.from(delta.toString());
-      expect(actualDelta).to.equal(amount);
+      for (testcase of lockETHTestCases) {
+        await testLockETH(testcase);
+      }
     });
   });
 
@@ -48,71 +50,149 @@ contract("TokenLocker", () => {
     // disable timeout
     this.timeout(0);
     it("Should lock erc20 token verified", async () => {
-      // deploy erc20
-      const factory = await ethers.getContractFactory(
-        "contracts/test/ERC20.sol:ERC20"
-      );
-      const erc20 = await factory.deploy();
-      await erc20.deployed();
-      log("erc20 deployed to:", erc20.address);
-
-      // get balance
-      const contractBalance = await erc20.callStatic.balanceOf(
-        tokenLocker.address
-      );
-      log(`tokenLocker contract erc20 balance: ${contractBalance.toString()}`);
-      const signerBalance = await erc20.callStatic.balanceOf(
-        await user.getAddress()
-      );
-      log(`signer erc20 balance: ${signerBalance.toString()}`);
-
-      // user should approve erc20 token to tokenLocker contract
-      const approveAmount =
-        "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
-      let res = await erc20.approve(tokenLocker.address, approveAmount);
-      let gasUsed = await waitingForGasUsed(provider, res);
-      log(`approve gasUsed: ${gasUsed.toString()}`);
-
-      // lock erc20
-      const amount = 789;
-      log(`lock erc20 token amount ${amount}`);
-      res = await tokenLocker.lockToken(erc20.address, amount, "ckb_address", "replayResistOutpoint");
-      gasUsed = await waitingForGasUsed(provider, res);
-      log(`gasUsed: ${gasUsed.toString()}`);
-
-      // asset expected amount == balance of contract delta
-      const contractBalanceAfter = await erc20.callStatic.balanceOf(
-        tokenLocker.address
-      );
-      log(`contractBalanceAfter ${contractBalanceAfter}`);
-      const delta = contractBalanceAfter - contractBalance;
-      const actualDelta = ethers.BigNumber.from(delta.toString());
-      expect(actualDelta).to.equal(amount);
+      for (testcase of lockTokenTestCases) {
+        await testLockToken(testcase);
+      }
     });
   });
 
-  describe("unlockETH", async function () {
+  describe("unlock token", async function () {
     // disable timeout
     this.timeout(0);
-    it("Should unlockETH verified", async () => {
-      // let defaultProvider = ethers.getDefaultProvider()
-      const defaultProvider = tokenLocker.signer.provider;
-      const contractBalance = await defaultProvider.getBalance(
-        tokenLocker.address
-      );
-
-      // unlockETH
-      const res = await tokenLocker.unlockToken([0], [0]);
-      const gasUsed = await waitingForGasUsed(provider, res);
-      log(`gasUsed: ${gasUsed.toString()}`);
-
-      // asset expected amount == balance of contract delta
-      const delta =
-        (await defaultProvider.getBalance(tokenLocker.address)) -
-        contractBalance;
-      const actualDelta = ethers.BigNumber.from(delta.toString());
-      const expected = ethers.BigNumber.from("-111100000000000000");
-      expect(actualDelta).to.equal(expected);
+    it("should decode burn tx verified", async () => {
+      for (t of decodeBurnTxTestCases) {
+        const burnResult = await tokenLocker.decodeBurnResult(t.txData);
+        expect(burnResult.token).to.equal(t.burnTokenAddress);
+        expect(burnResult.recipient).to.equal(t.recipientAddress);
+        expect(burnResult.amount.toNumber()).to.equal(t.burnAmount);
+      }
     });
+    // TODO test unlock
   });
 });
+
+async function testLockETH(testcase) {
+  const contractBalance = await provider.getBalance(tokenLocker.address);
+
+  // lockETH
+  const amount = ethers.utils.parseEther(testcase.lockAmount);
+  const res = await tokenLocker.lockETH(
+    testcase.ckbAddress,
+    testcase.replayResistOutpoint,
+    { value: amount }
+  );
+  const receipt = await waitingForReceipt(provider, res);
+  log(`gasUsed: ${receipt.gasUsed.toString()}`);
+
+  const {
+    tokenAddressTopic,
+    lockerAddressTopic,
+    ckbAddressTopic,
+    lockedAmount,
+    replayResistOutpoint,
+  } = parseLockedEvent(receipt.logs[0]);
+
+  expect(tokenAddressTopic).to.equal(
+    "0x0000000000000000000000000000000000000000"
+  );
+  expect(lockerAddressTopic).to.equal(user.address);
+  const hashCKBAddress = ethers.utils.keccak256(
+    ethers.utils.toUtf8Bytes(testcase.ckbAddress)
+  );
+  expect(ckbAddressTopic).to.equal(hashCKBAddress);
+  expect(lockedAmount).to.equal(amount);
+  expect(replayResistOutpoint).to.equal(testcase.replayResistOutpoint);
+
+  // locked token amount == delta balance of contract
+  const delta =
+    (await provider.getBalance(tokenLocker.address)) - contractBalance;
+  const actualDelta = ethers.BigNumber.from(delta.toString());
+  expect(actualDelta).to.equal(amount);
+}
+
+async function testLockToken(testcase) {
+  const factory = await ethers.getContractFactory(
+    "contracts/test/ERC20.sol:ERC20"
+  );
+  const erc20 = await factory.deploy();
+  await erc20.deployed();
+  log("erc20 deployed to:", erc20.address);
+
+  const contractBalance = await erc20.callStatic.balanceOf(tokenLocker.address);
+  log(`tokenLocker contract erc20 balance: ${contractBalance.toString()}`);
+  const signerBalance = await erc20.callStatic.balanceOf(
+    await user.getAddress()
+  );
+  log(`signer erc20 balance: ${signerBalance.toString()}`);
+
+  // user should approve erc20 token to tokenLocker contract
+  const approveAmount =
+    "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+  let res = await erc20.approve(tokenLocker.address, approveAmount);
+  let receipt = await waitingForReceipt(provider, res);
+  log(`approve gasUsed: ${receipt.gasUsed.toString()}`);
+
+  // lock erc20
+  const amount = testcase.lockAmount;
+  log(`lock erc20 token amount ${amount}`);
+  res = await tokenLocker.lockToken(
+    erc20.address,
+    amount,
+    testcase.ckbAddress,
+    testcase.replayResistOutpoint
+  );
+  receipt = await waitingForReceipt(provider, res);
+  log(`gasUsed: ${receipt.gasUsed.toString()}`);
+
+  const {
+    tokenAddressTopic,
+    lockerAddressTopic,
+    ckbAddressTopic,
+    lockedAmount,
+    replayResistOutpoint,
+  } = parseLockedEvent(receipt.logs[2]);
+
+  expect(tokenAddressTopic).to.equal(erc20.address);
+  expect(lockerAddressTopic).to.equal(user.address);
+  const hashCKBAddress = ethers.utils.keccak256(
+    ethers.utils.toUtf8Bytes(testcase.ckbAddress)
+  );
+  expect(ckbAddressTopic).to.equal(hashCKBAddress);
+  expect(lockedAmount).to.equal(amount);
+  expect(replayResistOutpoint).to.equal(testcase.replayResistOutpoint);
+
+  // locked token amount == delta balance of contract
+  const contractBalanceAfter = await erc20.callStatic.balanceOf(
+    tokenLocker.address
+  );
+  log(`contractBalanceAfter ${contractBalanceAfter}`);
+  const delta = contractBalanceAfter - contractBalance;
+  const actualDelta = ethers.BigNumber.from(delta.toString());
+  expect(actualDelta).to.equal(amount);
+}
+
+function parseLockedEvent(eventLog) {
+  const tokenAddressTopic = ethers.utils.defaultAbiCoder.decode(
+    ["address"],
+    eventLog.topics[1]
+  )[0];
+  const lockerAddressTopic = ethers.utils.defaultAbiCoder.decode(
+    ["address"],
+    eventLog.topics[2]
+  )[0];
+  const ckbAddressTopic = ethers.utils.defaultAbiCoder.decode(
+    ["bytes32"],
+    eventLog.topics[3]
+  )[0];
+  const eventData = ethers.utils.defaultAbiCoder.decode(
+    ["uint256", "string"],
+    eventLog.data
+  );
+  return {
+    tokenAddressTopic: tokenAddressTopic,
+    lockerAddressTopic: lockerAddressTopic,
+    ckbAddressTopic: ckbAddressTopic,
+    lockedAmount: eventData[0],
+    replayResistOutpoint: eventData[1],
+  };
+}
