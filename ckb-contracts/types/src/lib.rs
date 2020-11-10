186ed89610e53ac8116b6ecbf2b5c660f77981eb
jacobdenver007 <jacobdenver@163.com>
2020-11-10 15:01:14 +0800

    fix(ckb-contracts): add eth headers types

--- ckb-contracts/types/src/lib.rs
+++ ckb-contracts/types/src/lib.rs
@@ -2,10 +2,9 @@
 
 pub mod config;
 pub mod convert;
+pub mod eth_header_cell;
 pub mod eth_recipient_cell;
 pub mod generated;
-
-pub use generated::*;
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "std")] {
