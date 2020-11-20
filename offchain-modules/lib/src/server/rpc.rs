ec388c79ea38f721db2ffb1e8a0d33aeebae8075
JacobDenver007 <29848184+JacobDenver007@users.noreply.github.com>
2020-11-21 01:01:40 +0800

    feat(offchain-modules): add rpc lock and get_sudt_balance (#48)
    * fix(offline-modules): add get_sudt_balance and lock rpc
    
    * fix(offchain-modules): add rpc name

--- offchain-modules/lib/src/server/rpc.rs
+++ offchain-modules/lib/src/server/rpc.rs
@@ -1,18 +1,29 @@
-use super::types::BurnArgs;
-use crate::util::ckb_util::Generator;
-use crate::util::eth_util::convert_eth_address;
+use super::types::{BurnArgs, GetSudtBalanceArgs, LockArgs};
+use crate::util::ckb_util::{build_lockscript_from_address, Generator};
+use crate::util::eth_util::{
+    build_lock_eth_payload, build_lock_token_payload, convert_eth_address, make_transaction,
+};
 use crate::util::settings::Settings;
 use ckb_jsonrpc_types::TransactionView;
 use ckb_sdk::Address;
 use ckb_types::packed::Script;
+use ethabi::Token;
+use ethereum_tx_sign::RawTransaction;
 use jsonrpc_core::{IoHandler, Result};
 use jsonrpc_derive::rpc;
 use jsonrpc_http_server::ServerBuilder;
+use molecule::prelude::Entity;
 use std::str::FromStr;
+use web3::types::U256;
 
 #[rpc]
 pub trait Rpc {
+    #[rpc(name = "burn")]
     fn burn(&self, args: BurnArgs) -> Result<TransactionView>;
+    #[rpc(name = "lock")]
+    fn lock(&self, args: LockArgs) -> Result<RawTransaction>;
+    #[rpc(name = "get_sudt_balance")]
+    fn get_sudt_balance(&self, args: GetSudtBalanceArgs) -> Result<u128>;
 }
 
 pub struct RpcImpl {
@@ -75,6 +86,76 @@ impl Rpc for RpcImpl {
             .map_err(|e| jsonrpc_core::Error::invalid_params(format!("burn fail, err: {}", e)))?;
         let rpc_tx = ckb_jsonrpc_types::TransactionView::from(tx);
         Ok(rpc_tx)
+    }
+
+    fn get_sudt_balance(&self, args: GetSudtBalanceArgs) -> Result<u128> {
+        let token_address = convert_eth_address(args.token_address.as_str())
+            .map_err(|_| jsonrpc_core::Error::invalid_params("token address parse fail"))?;
+        let lock_contract_address = convert_eth_address(args.lock_contract_address.as_str())
+            .map_err(|_| jsonrpc_core::Error::invalid_params("lock contract address parse fail"))?;
+
+        let mut generator = self.get_generator()?;
+
+        let balance = generator
+            .get_sudt_balance(args.address, token_address, lock_contract_address)
+            .map_err(|e| {
+                jsonrpc_core::Error::invalid_params(format!("get_sudt_balance fail, err: {}", e))
+            })?;
+        Ok(balance)
+    }
+
+    fn lock(&self, args: LockArgs) -> Result<RawTransaction> {
+        let to = convert_eth_address(args.lock_contract_address.as_str())
+            .map_err(|_| jsonrpc_core::Error::invalid_params("lock contract address parse fail"))?;
+        let nonce = U256::from(args.nonce);
+        let gas_price = U256::from(args.gas_price);
+        let amount = U256::from(args.amount);
+        let bridge_fee = U256::from(args.bridge_fee);
+
+        let token_addr = convert_eth_address(&args.token_address)
+            .map_err(|_| jsonrpc_core::Error::invalid_params("token address parse fail"))?;
+        let recipient_lockscript = build_lockscript_from_address(&args.ckb_recipient_address)
+            .map_err(|_| jsonrpc_core::Error::invalid_params("ckb recipient address parse fail"))?;
+
+        let data = [
+            Token::Address(token_addr),
+            Token::Uint(amount),
+            Token::Uint(bridge_fee),
+            Token::Bytes(recipient_lockscript.as_slice().to_vec()),
+            Token::Bytes(hex::decode(args.replay_resist_outpoint).map_err(|e| {
+                jsonrpc_core::Error::invalid_params(format!(
+                    "decode replay_resist_outpoint fail, err: {}",
+                    e
+                ))
+            })?),
+            Token::Bytes(args.sudt_extra_data.as_bytes().to_vec()),
+        ];
+
+        let mut eth_value = amount;
+
+        let input_data = {
+            if token_addr.0 == [0u8; 20] {
+                let lock_eth_data = &data[2..];
+                build_lock_eth_payload(lock_eth_data).map_err(|e| {
+                    jsonrpc_core::Error::invalid_params(format!(
+                        "abi encode lock eth data fail, err: {}",
+                        e
+                    ))
+                })?
+            } else {
+                eth_value = U256::from(0);
+                build_lock_token_payload(&data).map_err(|e| {
+                    jsonrpc_core::Error::invalid_params(format!(
+                        "abi encode lock token data fail, err: {}",
+                        e
+                    ))
+                })?
+            }
+        };
+
+        Ok(make_transaction(
+            to, nonce, input_data, gas_price, eth_value,
+        ))
     }
 }
 
