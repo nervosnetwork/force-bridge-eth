021767c8b3c4093d2e08caf2ab446ce290d0319c
LeonLi000 <matrix.skygirl@gmail.com>
2020-11-23 20:17:22 +0800

    feat: update make file

--- demo/demo.sh
+++ demo/demo.sh
@@ -6,6 +6,10 @@ set -o xtrace
 export RUST_BACKTRACE=1
 export RUST_LOG=info,force=debug
 
+trap cleanup EXIT
+
+
+
 # project root directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 DATA_DIR="${DIR}"/demo/data
@@ -15,6 +19,17 @@ FORTH_ETH_CONFIG_PATH="${DATA_DIR}"/force-eth-config.json
 BRIDGE_CELL_CONFIG_PATH="${DATA_DIR}"/bridge-cell-config.json
 LOCK_TOKEN_PATH="${DATA_DIR}"/lock_token.log
 LOCK_ETH_PATH="${DATA_DIR}"/lock_eth.log
+
+cleanup() {
+    # Kill the relay service && docker instance that we started (if we started one and if it's still running).
+    ps aux | grep 'force-eth-cli ckb-relay' | grep -v grep | awk '{print $2}' | xargs kill -9
+    ps aux | grep 'force-eth-cli eth-relay' | grep -v grep | awk '{print $2}' | xargs kill -9
+
+    cd "$DIR"/docker
+#    docker-compose stop
+    docker ps -a | grep tockb | awk '{print $1}' | xargs docker stop
+    docker ps -a | grep tockb | awk '{print $1}' | xargs docker rm
+}
 
 cd "$DIR"/demo
 ${FORCE_CLI} dev-init -f
@@ -31,8 +46,8 @@ cd "$DIR"/demo
 
 # start relayer
 ${FORCE_CLI} init-ckb-light-contract -i 1 -f 500 -c 40000 --wait
-ps aux | grep 'force-eth-cli ckb-relay' | grep -v grep | awk '{print $2}' | xargs kill -9
-ps aux | grep 'force-eth-cli eth-relay' | grep -v grep | awk '{print $2}' | xargs kill -9
+#ps aux | grep 'force-eth-cli ckb-relay' | grep -v grep | awk '{print $2}' | xargs kill -9
+#ps aux | grep 'force-eth-cli eth-relay' | grep -v grep | awk '{print $2}' | xargs kill -9
 ${FORCE_CLI} ckb-relay -k privkeys/ckb2eth_relayer_key --per-amount 10 > data/ckb-relayer.log 2>&1 &
 ${FORCE_CLI} eth-relay > data/eth-relayer.log 2>&1 &
 
@@ -52,9 +67,9 @@ ${FORCE_CLI} create-bridge-cell --eth-token-address "${TOKEN_ADDRESS}" --recipie
 bridge_cell_outpoint=$(cat "${BRIDGE_CELL_CONFIG_PATH}" | jq -r .outpoint)
 ${FORCE_CLI} lock-token --ckb-recipient-address "${RECIPIENT_ADDR}" --replay-resist-outpoint "${bridge_cell_outpoint}" --token  "${TOKEN_ADDRESS}" --amount 100 --bridge-fee "${bridge_fee}" --sudt-extra-data sudt_extra_data --wait > "${LOCK_TOKEN_PATH}"
 lock_token_hash=`cat "${LOCK_TOKEN_PATH}"| awk '{print $5}'`
-${FORCE_CLI} mint --hash "${lock_token_hash}" --cell depend_on_eth_relay
+${FORCE_CLI} mint --hash "${lock_token_hash}"
 ${FORCE_CLI} query-sudt-blance --addr ${RECIPIENT_ADDR} --token-addr "${TOKEN_ADDRESS}"
 ${FORCE_CLI} transfer-from-ckb --ckb-privkey-path privkeys/ckb_key_recipient --burn-amount 2 --unlock-fee 1 --receive-addr 0x403A53A7Dfa7a4AB022e53FeFf11232b3140407d --wait --token-addr "${TOKEN_ADDRESS}"
 
 # kill relayer
-ps aux | grep 'force-eth-cli ckb-relay' | grep -v grep | awk '{print $2}' | xargs kill -9
+#ps aux | grep 'force-eth-cli ckb-relay' | grep -v grep | awk '{print $2}' | xargs kill -9
