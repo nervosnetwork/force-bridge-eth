0a5f465ee4e9639c7dc41605ef0d37781a1f329b
fpChan <fpchen@outlook.com>
2020-11-28 03:01:51 +0800

    fix: change script data type for issue#135

--- offchain-modules/lib/src/relay/eth_relay.rs
+++ offchain-modules/lib/src/relay/eth_relay.rs
@@ -4,7 +4,7 @@ use crate::util::eth_util::Web3Client;
 use crate::util::settings::Settings;
 use anyhow::{anyhow, Result};
 use ckb_sdk::{AddressPayload, SECP256K1};
-use ckb_types::core::{DepType, TransactionView};
+use ckb_types::core::{ScriptHashType, TransactionView};
 use ckb_types::packed::{Byte32, Script};
 use ckb_types::prelude::{Builder, Entity};
 use cmd_lib::run_cmd;
@@ -118,7 +118,7 @@ impl ETHRelayer {
                 )
                 .map_err(|err| anyhow::anyhow!(err))?,
             )
-            .hash_type(DepType::Code.into())
+            .hash_type(ScriptHashType::Data.into())
             .build();
 
         let lockscript = Script::new_builder()
@@ -129,7 +129,7 @@ impl ETHRelayer {
                 )
                 .map_err(|err| anyhow::anyhow!(err))?,
             )
-            .hash_type(DepType::Code.into())
+            .hash_type(ScriptHashType::Data.into())
             .build();
         let current_number = self.eth_client.client().eth().block_number().await?;
         let block = self.eth_client.get_block(current_number.into()).await?;
