beba530948e30c5367091ca505f724b4c5a632ea
huwenchao <me@huwenchao.com>
2020-12-03 21:42:35 +0800

    refactor relay proof

--- offchain-modules/lib/src/server/proof_relayer/handler.rs
+++ offchain-modules/lib/src/server/proof_relayer/handler.rs
@@ -1,15 +1,12 @@
+use crate::relay::eth_relay::wait_header_sync_success;
 use crate::server::proof_relayer::db::{update_eth_to_ckb_status, EthToCkbRecord};
-use crate::transfer::to_ckb::generate_eth_spv_proof_json;
+use crate::transfer::to_ckb::{generate_eth_spv_proof_json, send_eth_spv_proof_tx};
 use crate::transfer::to_eth::{get_ckb_proof_info, unlock, wait_block_submit};
 use crate::util::ckb_tx_generator::Generator;
 use crate::util::config::ForceConfig;
 use crate::util::eth_util::{convert_eth_address, convert_hex_to_h256, Web3Client};
 use anyhow::{anyhow, Result};
-use ckb_sdk::AddressPayload;
-use ckb_sdk::SECP256K1;
 use ckb_types::core::TransactionView;
-use ckb_types::packed::Script;
-use force_sdk::tx_helper::sign;
 use molecule::prelude::Entity;
 use secp256k1::SecretKey;
 use sqlx::SqlitePool;
@@ -90,17 +87,22 @@ pub async fn relay_eth_to_ckb_proof(
         eth_token_locker_addr.clone(),
     )
     .await?;
-    let from_public_key = secp256k1::PublicKey::from_secret_key(&SECP256K1, &from_privkey);
-    let address_payload = AddressPayload::from_pubkey(&from_public_key);
-    let from_lockscript = Script::from(&address_payload);
-    let unsigned_tx =
-        generator.generate_eth_spv_tx(config_path.clone(), from_lockscript, &eth_proof)?;
-    let tx =
-        sign(unsigned_tx, &mut generator.rpc_client, &from_privkey).map_err(|err| anyhow!(err))?;
-    let tx_hash = generator
-        .rpc_client
-        .send_transaction(tx.data())
-        .map_err(|err| anyhow!("Send transaction error: {}", err))?;
+    let force_config = ForceConfig::new(config_path.as_str())?;
+    let deployed_contracts = force_config
+        .deployed_contracts
+        .as_ref()
+        .ok_or_else(|| anyhow!("contracts should be deployed"))?;
+    wait_header_sync_success(
+        &mut generator,
+        deployed_contracts
+            .light_client_cell_script
+            .cell_script
+            .as_str(),
+        eth_proof.header_data.clone(),
+    )
+    .await?;
+    let tx_hash =
+        send_eth_spv_proof_tx(&mut generator, config_path, &eth_proof, from_privkey).await?;
     record.token_addr = Some(hex::encode(eth_proof.token.as_bytes()));
     record.ckb_recipient_lockscript = Some(hex::encode(eth_proof.recipient_lockscript));
     update_eth_to_ckb_status(db, &record).await?;
