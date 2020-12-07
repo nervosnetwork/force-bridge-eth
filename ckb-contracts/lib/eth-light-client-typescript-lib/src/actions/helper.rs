0aca6c834f3fb0874aec63f70c993ab6e62ae96c
LeonLi000 <matrix.skygirl@gmail.com>
2020-12-07 17:49:06 +0800

    feat: remove rust-ethash dependence

--- ckb-contracts/lib/eth-light-client-typescript-lib/src/actions/helper.rs
+++ ckb-contracts/lib/eth-light-client-typescript-lib/src/actions/helper.rs
@@ -2,7 +2,6 @@
 #![allow(clippy::all)]
 
 use crate::adapter::Adapter;
-use crate::debug;
 #[cfg(not(feature = "std"))]
 use alloc::vec;
 #[cfg(not(feature = "std"))]
@@ -10,12 +9,8 @@ use alloc::vec::Vec;
 
 use eth_spv_lib::eth_types::*;
 
-use force_eth_types::{
-    eth_header_cell::ETHHeaderCellDataView,
-    generated::eth_header_cell::{
-        DagsMerkleRootsReader, DoubleNodeWithMerkleProofReader, ETHLightClientWitnessReader,
-        MerkleProofVecReader,
-    },
+use force_eth_types::generated::eth_header_cell::{
+    DagsMerkleRootsReader, DoubleNodeWithMerkleProofReader, ETHLightClientWitnessReader,
 };
 use molecule::prelude::Reader;
 
@@ -63,106 +58,106 @@ impl DoubleNodeWithMerkleProof {
     }
 }
 
-fn verify_merkle_proof<T: Adapter>(
-    data_loader: T,
-    witness: ETHLightClientWitnessReader,
-    output: &ETHHeaderCellDataView,
-    header: &BlockHeader,
-    prev: Option<BlockHeader>,
-) {
-    if MerkleProofVecReader::verify(&output.merkle_proofs, false).is_err() {
-        panic!("verify_merkle_proof, invalid output.merkle_proof");
-    }
-    let merkle_proof_vec = MerkleProofVecReader::new_unchecked(&output.merkle_proofs);
-    let mut proofs = vec![];
-    let merkle_proofs = merkle_proof_vec.get_unchecked(merkle_proof_vec.len() - 1);
-    for i in 0..merkle_proofs.len() {
-        let proof_raw = merkle_proofs.get_unchecked(i).raw_data();
-        let proof = parse_proof(proof_raw);
-        proofs.push(proof);
-    }
+// fn verify_merkle_proof<T: Adapter>(
+//     data_loader: T,
+//     witness: ETHLightClientWitnessReader,
+//     output: &ETHHeaderCellDataView,
+//     header: &BlockHeader,
+//     prev: Option<BlockHeader>,
+// ) {
+//     if MerkleProofVecReader::verify(&output.merkle_proofs, false).is_err() {
+//         panic!("verify_merkle_proof, invalid output.merkle_proof");
+//     }
+//     let merkle_proof_vec = MerkleProofVecReader::new_unchecked(&output.merkle_proofs);
+//     let mut proofs = vec![];
+//     let merkle_proofs = merkle_proof_vec.get_unchecked(merkle_proof_vec.len() - 1);
+//     for i in 0..merkle_proofs.len() {
+//         let proof_raw = merkle_proofs.get_unchecked(i).raw_data();
+//         let proof = parse_proof(proof_raw);
+//         proofs.push(proof);
+//     }
+//
+//     // parse dep data
+//     let merkle_root = parse_dep_data(data_loader, witness, header.number);
+//
+//     if !verify_header(&header, prev, merkle_root, &proofs) {
+//         panic!("verify_witness, verify header fail");
+//     }
+// }
 
-    // parse dep data
-    let merkle_root = parse_dep_data(data_loader, witness, header.number);
-
-    if !verify_header(&header, prev, merkle_root, &proofs) {
-        panic!("verify_witness, verify header fail");
-    }
-}
-
-pub fn verify_header(
-    header: &BlockHeader,
-    prev: Option<BlockHeader>,
-    merkle_root: H128,
-    dag_nodes: &[DoubleNodeWithMerkleProof],
-) -> bool {
-    let (_mix_hash, result) = hashimoto_merkle(
-        &header.partial_hash.unwrap(),
-        &header.nonce,
-        header.number,
-        merkle_root,
-        dag_nodes,
-    );
-    debug!("verify_header header: {:?}", header);
-    // See YellowPaper formula (50) in section 4.3.4
-    // 1. Simplified difficulty check to conform adjusting difficulty bomb
-    // 2. Added condition: header.parent_hash() == prev.hash()
-    let result = U256((result.0).0.into()) < U256(ethash::cross_boundary(header.difficulty.0))
-        && (header.difficulty < header.difficulty * 101 / 100
-            && header.difficulty > header.difficulty * 99 / 100)
-        && header.gas_used <= header.gas_limit
-        && header.gas_limit >= U256(5000.into())
-        && header.extra_data.len() <= 32;
-    match prev {
-        Some(prev) => {
-            debug!("verify_header prev: {:?}", prev);
-            result
-                && header.gas_limit < prev.gas_limit * 1025 / 1024
-                && header.gas_limit > prev.gas_limit * 1023 / 1024
-                && header.timestamp > prev.timestamp
-                && header.number == prev.number + 1
-                && header.parent_hash == prev.hash.unwrap()
-        }
-        None => result,
-    }
-}
+// pub fn verify_header(
+//     header: &BlockHeader,
+//     prev: Option<BlockHeader>,
+//     merkle_root: H128,
+//     dag_nodes: &[DoubleNodeWithMerkleProof],
+// ) -> bool {
+//     let (_mix_hash, result) = hashimoto_merkle(
+//         &header.partial_hash.unwrap(),
+//         &header.nonce,
+//         header.number,
+//         merkle_root,
+//         dag_nodes,
+//     );
+//     debug!("verify_header header: {:?}", header);
+//     // See YellowPaper formula (50) in section 4.3.4
+//     // 1. Simplified difficulty check to conform adjusting difficulty bomb
+//     // 2. Added condition: header.parent_hash() == prev.hash()
+//     let result = U256((result.0).0.into()) < U256(ethash::cross_boundary(header.difficulty.0))
+//         && (header.difficulty < header.difficulty * 101 / 100
+//             && header.difficulty > header.difficulty * 99 / 100)
+//         && header.gas_used <= header.gas_limit
+//         && header.gas_limit >= U256(5000.into())
+//         && header.extra_data.len() <= 32;
+//     match prev {
+//         Some(prev) => {
+//             debug!("verify_header prev: {:?}", prev);
+//             result
+//                 && header.gas_limit < prev.gas_limit * 1025 / 1024
+//                 && header.gas_limit > prev.gas_limit * 1023 / 1024
+//                 && header.timestamp > prev.timestamp
+//                 && header.number == prev.number + 1
+//                 && header.parent_hash == prev.hash.unwrap()
+//         }
+//         None => result,
+//     }
+// }
 
 /// Verify merkle paths to the DAG nodes.
-fn hashimoto_merkle(
-    header_hash: &H256,
-    nonce: &H64,
-    header_number: u64,
-    merkle_root: H128,
-    nodes: &[DoubleNodeWithMerkleProof],
-) -> (H256, H256) {
-    let mut index = 0;
-    let pair = ethash::hashimoto_with_hasher(
-        header_hash.0,
-        nonce.0,
-        ethash::get_full_size(header_number as usize / 30000),
-        |offset| {
-            let idx = index;
-            debug!("hashimoto_with_hasher index: {}", index);
-            index += 1;
-            // Each two nodes are packed into single 128 bytes with Merkle proof
-            let node = &nodes[idx / 2];
-            if idx % 2 == 0 {
-                // Divide by 2 to adjust offset for 64-byte words instead of 128-byte
-                assert_eq!(merkle_root, node.apply_merkle_proof((offset / 2) as u64));
-            };
-
-            // Reverse each 32 bytes for ETHASH compatibility
-            let mut data = (node.dag_nodes[idx % 2].0).0;
-            data[..32].reverse();
-            data[32..].reverse();
-            data.into()
-        },
-        keccak256,
-        keccak512,
-    );
-
-    (H256(pair.0), H256(pair.1))
-}
+// fn hashimoto_merkle(
+//     header_hash: &H256,
+//     nonce: &H64,
+//     header_number: u64,
+//     merkle_root: H128,
+//     nodes: &[DoubleNodeWithMerkleProof],
+// ) -> (H256, H256) {
+//     let mut index = 0;
+//     let pair = ethash::hashimoto_with_hasher(
+//         header_hash.0,
+//         nonce.0,
+//         ethash::get_full_size(header_number as usize / 30000),
+//         |offset| {
+//             let idx = index;
+//             debug!("hashimoto_with_hasher index: {}", index);
+//             index += 1;
+//             // Each two nodes are packed into single 128 bytes with Merkle proof
+//             let node = &nodes[idx / 2];
+//             if idx % 2 == 0 {
+//                 // Divide by 2 to adjust offset for 64-byte words instead of 128-byte
+//                 assert_eq!(merkle_root, node.apply_merkle_proof((offset / 2) as u64));
+//             };
+//
+//             // Reverse each 32 bytes for ETHASH compatibility
+//             let mut data = (node.dag_nodes[idx % 2].0).0;
+//             data[..32].reverse();
+//             data[32..].reverse();
+//             data.into()
+//         },
+//         keccak256,
+//         keccak512,
+//     );
+//
+//     (H256(pair.0), H256(pair.1))
+// }
 
 fn parse_proof(proof_raw: &[u8]) -> DoubleNodeWithMerkleProof {
     if DoubleNodeWithMerkleProofReader::verify(&proof_raw, false).is_err() {
