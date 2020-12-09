4075af7533c18ea8330a074d439201034786c662
fpChan <fpchen@outlook.com>
2020-12-09 18:38:44 +0800

    feat: add cargo-tarpaulin and types coverage

--- ckb-contracts/ckb_script_coverage.sh
+++ ckb-contracts/ckb_script_coverage.sh
@@ -3,6 +3,6 @@ lib_dirs=("lib/eth-bridge-lockscript-lib" "lib/eth-light-client-lockscript-lib" 
 for dir in "${lib_dirs[@]}"
 do
 	if [ -d $dir ]; then
-		cd $dir && cargo tarpaulin --exclude-files ../../types/* --exclude-files ../../eth-spv-lib/* --exclude-files ../../tests/* -o html -o xml --output-dir ../../target/tarpaulin/$dir && bash <(curl -s https://codecov.io/bash) -f ../../target/tarpaulin/$dir/cobertura.xml && cd ../../
+		cd $dir && cargo tarpaulin --exclude-files ../../tests/* -o html -o xml --output-dir ../../target/tarpaulin/$dir && bash <(curl -s https://codecov.io/bash) -f ../../target/tarpaulin/$dir/cobertura.xml && cd ../../
 	fi
 done
