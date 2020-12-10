d577ec3e6cccaae87e031e765bc428b58446619d
jacobdenver007 <jacobdenver@163.com>
2020-12-10 22:19:49 +0800

    fix(ckb-contracts): add ut for bridge typescript

--- ckb-contracts/lib/eth-bridge-typescript-lib/src/test.rs
+++ ckb-contracts/lib/eth-bridge-typescript-lib/src/test.rs
@@ -2,8 +2,9 @@ use crate::_verify;
 use crate::adapter::*;
 use ckb_std::ckb_constants::Source;
 use ckb_std::ckb_types::packed::Script;
+use ckb_std::ckb_types::prelude::Pack;
+use ckb_std::error::SysError;
 use force_eth_types::generated::basic::Byte32;
-use force_eth_types::generated::eth_bridge_lock_cell::ETHBridgeLockArgs;
 use force_eth_types::generated::eth_bridge_type_cell::{ETHBridgeTypeArgs, ETHBridgeTypeData};
 use force_eth_types::generated::witness::MintTokenWitness;
 use molecule::prelude::Byte;
@@ -19,13 +20,14 @@ fn test_manage_mode() {
         .times(1)
         .returning(move || Ok(witness.as_bytes()));
 
+    #[allow(clippy::redundant_closure)]
     mock.expect_load_script_args()
         .times(1)
-        .returning(move || ETHBridgeTypeArgs::default());
+        .returning(|| ETHBridgeTypeArgs::default());
 
     mock.expect_load_data()
         .times(1)
-        .returning(move || Some(ETHBridgeTypeData::default()));
+        .returning(|| Some(ETHBridgeTypeData::default()));
 
     mock.expect_lock_script_exists_in_inputs()
         .times(1)
@@ -45,6 +47,7 @@ fn test_manage_mode_when_lock_script_not_exist_in_inputs() {
         .times(1)
         .returning(move || Ok(witness.as_bytes()));
 
+    #[allow(clippy::redundant_closure)]
     mock.expect_load_script_args()
         .times(1)
         .returning(move || ETHBridgeTypeArgs::default());
@@ -60,11 +63,17 @@ fn test_manage_mode_when_lock_script_not_exist_in_inputs() {
     _verify(mock);
 }
 
-#[test]
-fn test_mint_token() {
-    /// assume correct input bridge lock hash is [100u8; 32]
-    /// assume correct input bridge type hash is [300u8; 32]
-    /// assume sudt code hash is [1000u8;32]
+fn generate_mint_token_params() -> MockAdapter {
+    let correct_input_lock_hash = [100u8; 32];
+    let correct_recipient_lock_hash = [101u8; 32];
+    let correct_input_type_hash = [102u8; 32];
+    let correct_sudt_code_hash = [103u8; 32];
+    let correct_owner_lockscript = [
+        53u8, 0, 0, 0, 16, 0, 0, 0, 48, 0, 0, 0, 49, 0, 0, 0, 230, 131, 176, 65, 57, 52, 71, 104,
+        52, 132, 153, 194, 62, 177, 50, 109, 90, 82, 214, 219, 0, 108, 13, 47, 236, 224, 10, 131,
+        31, 54, 96, 215, 0, 0, 0, 0, 0,
+    ];
+
     let mut mock = MockAdapter::new();
 
     mock.expect_load_input_witness_args()
@@ -73,39 +82,102 @@ fn test_mint_token() {
 
     mock.expect_load_script_args().times(1).returning(move || {
         ETHBridgeTypeArgs::new_builder()
-            .bridge_lock_hash([100u8; 32].pack().into())
+            .bridge_lock_hash(Byte32::new_unchecked(
+                correct_input_lock_hash.to_vec().into(),
+            ))
+            .recipient_lock_hash(Byte32::new_unchecked(
+                correct_recipient_lock_hash.to_vec().into(),
+            ))
             .build()
     });
 
-    mock.expect_load_data()
-        .times(1)
-        .returning(move || Some(ETHBridgeTypeData::default()));
+    mock.expect_load_data().times(1).returning(move || {
+        Some(
+            ETHBridgeTypeData::new_builder()
+                .owner_lock_script(correct_owner_lockscript.to_vec().into())
+                .fee(10u128.into())
+                .build(),
+        )
+    });
 
     mock.expect_load_cell_lock_hash()
         .times(2)
-        .returning(|_, source| {
+        .returning(move |_, source| {
             if source == Source::Input {
-                Ok([100u8; 32])
+                Ok(correct_input_lock_hash)
             } else {
-                Ok([200u8; 32])
+                Ok(correct_recipient_lock_hash)
             }
         });
 
     mock.expect_load_cell_type_hash()
         .times(1)
-        .returning(|_, _| Ok(Some([300u8; 32])));
+        .returning(move |_, _| Ok(Some(correct_input_type_hash)));
 
     mock.expect_load_script_hash()
         .times(1)
-        .returning(|_, _| Ok([300u8; 32]));
+        .returning(move || correct_input_type_hash);
 
     mock.expect_get_associated_udt_script()
         .times(1)
-        .returning(|_| Script::new_builder().code_hash([1000u8; 32].pack().into()));
+        .returning(move |_| {
+            Script::new_builder()
+                .code_hash(correct_sudt_code_hash.clone().pack())
+                .build()
+        });
+
+    mock.expect_load_cell_lock_script()
+        .times(1)
+        .returning(move |_, _| Ok(Script::from_slice(&correct_owner_lockscript.clone()).unwrap()));
+
+    mock.expect_load_cell_data()
+        .times(1)
+        .returning(|_, _| Ok(10u128.to_le_bytes().to_vec()));
+
+    mock
+}
+
+#[test]
+fn test_mint_token() {
+    let correct_sudt_code_hash = [103u8; 32];
+
+    let mut mock = generate_mint_token_params();
 
     mock.expect_load_cell_type()
-        .times(1)
-        .returning(|_| Script::new_builder().code_hash([1000u8; 32].pack().into()));
+        .times(4)
+        .returning(move |index, _| {
+            if index == 0 || index == 1 {
+                Ok(Some(
+                    Script::new_builder()
+                        .code_hash(correct_sudt_code_hash.clone().pack())
+                        .build(),
+                ))
+            } else if index == 2 {
+                Ok(Some(Script::default()))
+            } else {
+                Err(SysError::IndexOutOfBound)
+            }
+        });
+
+    _verify(mock);
+}
+
+#[test]
+#[should_panic(expected = "mint more sudt than expected")]
+fn test_mint_token_when_mint_more_sudt_than_expected() {
+    let correct_sudt_code_hash = [103u8; 32];
+
+    let mut mock = generate_mint_token_params();
+
+    mock.expect_load_cell_type()
+        .times(3)
+        .returning(move |_, _| {
+            Ok(Some(
+                Script::new_builder()
+                    .code_hash(correct_sudt_code_hash.clone().pack())
+                    .build(),
+            ))
+        });
 
     _verify(mock);
 }
