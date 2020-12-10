88780a3ad4be1fb0c120d7309ecfc9e92ebdf707
LeonLi000 <matrix.skygirl@gmail.com>
2020-12-11 00:40:07 +0800

    minor change

--- offchain-modules/start-services.sh
+++ offchain-modules/start-services.sh
@@ -38,12 +38,12 @@ if [ "${FORCE_NETWORK}" = "" ]
 then
   ${FORCE_CLI} init-ckb-light-contract -k 0 -f 500 -c 40000 --wait
   ${FORCE_CLI} ckb-relay -k 1 --per-amount 5 > ${FORCE_LOG_PATH}/ckb-relayer.log 2>&1 &
-#  echo "" |  ${CKB_CLI} account import --privkey-path ${PRIVKEY_PATH}/ckb1
-#  echo "" |  ${CKB_CLI} account import --privkey-path ${PRIVKEY_PATH}/ckb2
-  ${CKB_CLI} tx build-multisig-address --sighash-address ckt1qyqyph8v9mclls35p6snlaxajeca97tc062sa5gahk ckt1qyqvsv5240xeh85wvnau2eky8pwrhh4jr8ts8vyj37 --threshold 2 > mutisig.log
-  lock_args=`cat mutisig.log| awk '{print $2}'| awk 'NR==1' `
+  echo "" |  ${CKB_CLI} account import --privkey-path ${PRIVKEY_PATH}/ckb1
+  echo "" |  ${CKB_CLI} account import --privkey-path ${PRIVKEY_PATH}/ckb2
+  ${CKB_CLI} tx build-multisig-address --sighash-address ckt1qyqyph8v9mclls35p6snlaxajeca97tc062sa5gahk ckt1qyqvsv5240xeh85wvnau2eky8pwrhh4jr8ts8vyj37 --threshold 2 > ${FORCE_LOG_PATH}/mutisig.log
+  lock_args=`cat ${FORCE_LOG_PATH}/mutisig.log| awk '{print $2}'| awk 'NR==1' `
   echo $lock_args
-  address=`cat mutisig.log| awk '{print $2}'| awk 'NR==4' `
+  address=`cat ${FORCE_LOG_PATH}/mutisig.log| awk '{print $2}'| awk 'NR==4' `
   echo $address
   echo "" |  ${CKB_CLI} wallet transfer --capacity 9000000000 --to-address $address  --from-account ckt1qyqyph8v9mclls35p6snlaxajeca97tc062sa5gahk --tx-fee 0.1
   sleep 20
