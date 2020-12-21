c9f64b509ce81ae45c8d3288434eb60646104ab8
huwenchao <me@huwenchao.com>
2020-12-21 22:38:41 +0800

    get light client back

--- ckb-contracts/lib/eth-light-client-typescript-lib/src/lib.rs
+++ ckb-contracts/lib/eth-light-client-typescript-lib/src/lib.rs
@@ -1,42 +1,20 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub mod actions;
-pub mod adapter;
-pub mod debug;
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+extern crate no_std_compat as std;
 
-pub use adapter::Adapter;
-
-cfg_if::cfg_if! {
-    if #[cfg(feature = "std")] {
-    } else {
-        extern crate alloc;
-    }
-}
+use contracts_helper::data_loader::DataLoader;
+use contracts_helper::debug;
+use force_eth_types::generated::witness::MintTokenWitnessReader;
+use molecule::prelude::{Entity, Reader};
 
 #[cfg(target_arch = "riscv64")]
 pub fn verify() -> i8 {
-    let chain = adapter::chain::ChainAdapter {};
-    _verify(chain)
-}
-
-pub fn _verify<T: Adapter>(data_loader: T) -> i8 {
-    // actions::verify_add_headers(data_loader);
+    _verify(adapter);
     0
 }
 
-#[cfg(test)]
-mod tests {
-    use super::_verify;
-    use crate::adapter::*;
-
-    #[test]
-    #[should_panic]
-    fn mock_panic_when_input_and_output_is_none() {
-        let mut mock = MockAdapter::new();
-        mock.expect_load_data_from_source()
-            .times(2)
-            .returning(|_| None);
-        let return_code = _verify(mock);
-        assert_eq!(return_code, 0);
-    }
+pub fn _verify<T: DataLoader>(data_loader: T) {
+    // todo
 }
