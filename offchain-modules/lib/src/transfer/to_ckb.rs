906c0e1a4aabefe91d853672466d5c8534b81aa2
LeonLi000 <matrix.skygirl@gmail.com>
2021-01-23 16:06:21 +0800

    optimize eth_indexer

--- offchain-modules/lib/src/transfer/to_ckb.rs
+++ offchain-modules/lib/src/transfer/to_ckb.rs
@@ -7,8 +7,8 @@ use crate::util::config::{
     CellScript, DeployedContracts, ForceConfig, MultisigConf, OutpointConf, ScriptConf,
 };
 use crate::util::eth_util::{
-    build_lock_eth_payload, build_lock_token_payload, convert_eth_address, parse_private_key,
-    Web3Client,
+    build_lock_eth_payload, build_lock_token_payload, convert_eth_address, convert_hex_to_h256,
+    parse_private_key, Web3Client,
 };
 use anyhow::{anyhow, Result};
 use ckb_hash::{blake2b_256, new_blake2b};
@@ -223,7 +223,7 @@ pub async fn generate_eth_spv_proof_json(
     };
     let eth_spv_proof = eth_spv_proof_retry(3)?;
     to_eth_spv_proof_json(
-        hash.clone(),
+        // hash.clone(),
         eth_spv_proof,
         eth_token_locker_addr,
         ethereum_rpc_url,
@@ -232,21 +232,27 @@ pub async fn generate_eth_spv_proof_json(
 }
 
 pub async fn to_eth_spv_proof_json(
-    hash: String,
+    // hash: String,
     eth_spv_proof: EthSpvProof,
     eth_token_locker_addr: String,
     ethereum_rpc_url: String,
 ) -> Result<ETHSPVProofJson> {
-    let header_rlp = get_header_rlp(ethereum_rpc_url.clone(), eth_spv_proof.block_hash).await?;
-    info!("tx: {:?}, eth_spv_proof: {:?}", hash, eth_spv_proof);
-    let hash_str = hash.clone();
+    let header_rlp = get_header_rlp(
+        ethereum_rpc_url.clone(),
+        convert_hex_to_h256(eth_spv_proof.block_hash.as_str())?,
+    )
+    .await?;
+    info!(
+        "tx: {:?}, eth_spv_proof: {:?}",
+        eth_spv_proof.tx_hash, eth_spv_proof
+    );
+    let hash_str = eth_spv_proof.tx_hash;
     let log_index = eth_spv_proof.log_index;
     let eth_rpc_url = ethereum_rpc_url.clone();
     let proof_hex = run_fun! {
     node eth-proof/index.js proof --hash ${hash_str} --index ${log_index} --url ${eth_rpc_url}}
     .unwrap();
     let proof_json: Value = serde_json::from_str(&proof_hex).unwrap();
-    info!("tx: {:?}, generate proof json: {:?}", hash, proof_json);
     // TODO: refactor to parse with static struct instead of dynamic parsing
     let mut proof_vec = vec![];
     for item in proof_json["proof"].as_array().unwrap() {
