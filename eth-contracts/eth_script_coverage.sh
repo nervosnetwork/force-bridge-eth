93ce9f807b92be72f48547f285813223652fd7f8
huwenchao <me@huwenchao.com>
2021-01-25 17:13:54 +0800

    fix ci

--- eth-contracts/eth_script_coverage.sh
+++ eth-contracts/eth_script_coverage.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-
-if [ -z "$CODECOV_TOKEN" ]
-then
-  echo "CODECOV_TOKEN not set, check https://app.codecov.io/gh/nervosnetwork/force-bridge-eth/settings"
-  exit 1
-fi
-
+echo $CODECOV_TOKEN
 npx hardhat coverage
 bash <(curl -s https://codecov.io/bash) -f ./coverage.json
