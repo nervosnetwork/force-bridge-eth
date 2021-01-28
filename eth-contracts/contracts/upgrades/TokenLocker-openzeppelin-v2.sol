baf8256d4cc552889c3a1401953a37b1315ec767
Ethan Hu <cosinlink@163.com>
2021-01-29 00:19:05 +0800

    feat: add coverage on openzeppelin upgradeable contracts (#193)

--- eth-contracts/contracts/upgrades/TokenLocker-openzeppelin-v2.sol
+++ eth-contracts/contracts/upgrades/TokenLocker-openzeppelin-v2.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.0;
 pragma abicoder v2;
 
-import {CKBCrypto} from "./libraries/CKBCrypto.sol";
-import {TypedMemView} from "./libraries/TypedMemView.sol";
-import {SafeMath} from "./libraries/SafeMath.sol";
-import {CKBTxView} from "./libraries/CKBTxView.sol";
-import {ViewSpv} from "./libraries/ViewSpv.sol";
-import {Address} from "./libraries/Address.sol";
-import {SafeERC20} from "./libraries/SafeERC20.sol";
-import {IERC20} from "./interfaces/IERC20.sol";
-import {ICKBSpv} from "./interfaces/ICKBSpv.sol";
+import {CKBCrypto} from "../libraries/CKBCrypto.sol";
+import {TypedMemView} from "../libraries/TypedMemView.sol";
+import {SafeMath} from "../libraries/SafeMath.sol";
+import {CKBTxView} from "../libraries/CKBTxView.sol";
+import {ViewSpv} from "../libraries/ViewSpv.sol";
+import {Address} from "../libraries/Address.sol";
+import {SafeERC20} from "../libraries/SafeERC20.sol";
+import {IERC20} from "../interfaces/IERC20.sol";
+import {ICKBSpv} from "../interfaces/ICKBSpv.sol";
 
 contract TokenLocker {
     using SafeMath for uint256;
