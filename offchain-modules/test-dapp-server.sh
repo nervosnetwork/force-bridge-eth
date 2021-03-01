717ea2ee3f940652d86dbdd11a81bacb1f9631b4
LeonLi000 <matrix.skygirl@gmail.com>
2021-03-01 13:19:08 +0800

    minor change

--- offchain-modules/test-dapp-server.sh
+++ offchain-modules/test-dapp-server.sh
@@ -99,10 +99,8 @@ start_header_relay(){
 
 
 stress_test(){
-    cd ${PROJECT_DIR}/demo
-#    && rm -rf dex-crosschain-bot
-#    git clone https://github.com/fpChan/dex-crosschain-bot &&
-    cd dex-crosschain-bot && yarn
+    cd ${PROJECT_DIR}/demo && rm -rf dex-crosschain-bot
+    git clone https://github.com/fpChan/dex-crosschain-bot && cd dex-crosschain-bot  && git checkout local && yarn
     yarn send && rm -rf dex-crosschain-bot
 }
 
