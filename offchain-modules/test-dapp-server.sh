0248044e77c8a90fcd4ef5f54d0a1afd11dda321
方平 <fpchen@outlook.com>
2021-03-16 08:56:13 +0800

    ci: update stress test

--- offchain-modules/test-dapp-server.sh
+++ offchain-modules/test-dapp-server.sh
@@ -103,7 +103,7 @@ start_header_relay(){
 
 stress_test(){
     cd ${PROJECT_DIR}/demo && rm -rf dex-crosschain-bot
-    git clone https://github.com/fpChan/dex-crosschain-bot && cd dex-crosschain-bot  && git checkout local-test && yarn
+    git clone https://github.com/fpChan/dex-crosschain-bot && cd dex-crosschain-bot  && git checkout local && yarn
     yarn send && rm -rf dex-crosschain-bot
 }
 
